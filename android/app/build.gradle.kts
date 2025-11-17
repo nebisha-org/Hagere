a11d9a35f7fe8cd5ca768fe34b32e92ea129b72e
nebi <nex.shaw.jobs@gmail.com>
2025-11-17 16:50:08 -0500

    Fix Android NDK config and release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.agerelige_flutter_client"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"   
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -26,8 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = 1 //flutter.versionCode
+        versionName = "0.1.0" //flutter.versionName
     }
 
     buildTypes {
